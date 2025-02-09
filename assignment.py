0e4fa7515a2ada71305f7c9112ba2303feb7b695
Emmanuel1222 <eisraeljames@gmail.com>
2025-02-09 16:13:04 +0300

    add changes

--- assignment.py
+++ assignment.py
@@ -1,12 +1,16 @@
 import numpy as np
 import pandas as pd
-
+import assignment
 def create_1d_array():
+
     """
     Create a 1D NumPy array with values [1, 2, 3, 4, 5]
     Returns:
         numpy.ndarray: 1D array
     """
+    arr_1d = np.array([1, 2, 3, 4, 5])
+    return arr_1d
+    print(arr_1d)
     pass
 
 def create_2d_array():
@@ -15,6 +19,9 @@ def create_2d_array():
     Returns:
         numpy.ndarray: 2D array
     """
+    array_2D = np.array([[1, 2,3], [4, 5, 6], [7, 8, 9]])
+    return array_2D
+    print(array_2D)
     pass
 
 def array_operations(arr):
@@ -26,7 +33,12 @@ def array_operations(arr):
     Returns:
         tuple: (mean, std_dev, max_value)
     """
-    pass
+    array_operations = np.array([2, 3, 4, 5])
+    mean = np.mean(array_operations)
+    std_dev = np.std(array_operations)
+    max_value = np.max(array_operations)
+    return (mean, std_dev, max_value)
+print(array_operations([1, 2, 3, 4, 5]))
 
 def read_csv_file(filepath):
     """
@@ -36,7 +48,9 @@ def read_csv_file(filepath):
     Returns:
         pandas.DataFrame: Loaded dataframe
     """
-    pass
+    data = pd.read_csv ("C:/Users/emmanueljames/Desktop/Hesabu/numpy-pandas-hisabati/data/sample-data.csv")
+    data_frame = pd.read_csv(data)
+    print (data)
 
 def handle_missing_values(df):
     """
@@ -46,6 +60,13 @@ def handle_missing_values(df):
     Returns:
         pandas.DataFrame: Cleaned dataframe
     """
+    df.isnull().sum()
+    for column in df.columns:
+        if df[column].dtype in ['int64', 'float']:
+            df[column].fillna(df[column].mean()), inplace = True
+        else:
+            df[column].fillna(df[column].mode()[0], inplace = True)
+            return df
     pass
 
 def select_data(df):
@@ -54,7 +75,12 @@ def select_data(df):
     Returns:
         pandas.DataFrame: Selected data
     """
-    pass
+    selected_columns = df[['Name' ,'Age','Salary',]]
+    print("Selected columns: ",selected_columns.head())
+    
+    # specific rows selected
+    selected_rows = selected_columns.loc[selected_columns['Age']>25]
+    return selected_rows
 
 def rename_columns(df):
     """
@@ -62,4 +88,10 @@ def rename_columns(df):
     Returns:
         pandas.DataFrame: DataFrame with renamed columns
     """
-    pass
+    renamed_df = df.rename(columns = columns_rename)
+    columns_rename = { 
+        "Name": "Office Name", 
+        "Salary": "Monthly Payment"}, inplace = True
+    return columns_rename
+
+
